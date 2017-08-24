4af4af65b20980f28e8fcb2c1867b4f607a6affe
zhouzhenhui <1767608686@qq.con>
2017-08-24 10:11:12 +0800

    定位

--- app/src/main/java/test/com/baidumap/MainActivity.java
+++ app/src/main/java/test/com/baidumap/MainActivity.java
@@ -1,6 +1,8 @@
 package test.com.baidumap;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 
 import com.baidu.location.BDLocation;
@@ -15,62 +17,99 @@ import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
 
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
 
 
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks{
+
+    private static final int BAIDU_READ_PHONE_STATE =100;
     private LocationClient mLocClient;
     public MyLocationListenner myListener = new MyLocationListenner();
     private MapView mMapView = null;
     private BaiduMap mBaiduMap = null;
 
+    String[] s = new String[]{Manifest.permission.READ_PHONE_STATE,Manifest.permission.ACCESS_COARSE_LOCATION ,
+            Manifest.permission.ACCESS_FINE_LOCATION ,Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // 在使用SDK各组件之前初始化context信息，传入ApplicationContext
         // 注意该方法要再setContentView方法之前实现
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
-        //获取地图控件引用
         mMapView = (MapView) findViewById(R.id.id_bmapView);
-
-
-        //定位
         mBaiduMap = mMapView.getMap();
         //普通地图
-        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
 
-//        //定义Maker坐标点
-//        LatLng point = new LatLng(116.306533,40.048396);
-////构建Marker图标
-//        BitmapDescriptor bitmap = BitmapDescriptorFactory
-//                .fromResource(R.mipmap.ic_launcher);
-////构建MarkerOption，用于在地图上添加Marker
-//        OverlayOptions option = new MarkerOptions()
-//                .position(point)
-//                .icon(bitmap);
-////在地图上添加Marker，并显示
-//        mBaiduMap.addOverlay(option);
+        methodRequiresTwoPermission();
 
 
-        // 开启定位图层
-        mBaiduMap.setMyLocationEnabled(true);
-        mLocClient = new LocationClient(this);  //定位用到的一个类
-        mLocClient.registerLocationListener(myListener); //注册监听
 
-        ///LocationClientOption类用来设置定位SDK的定位方式，
-        LocationClientOption option = new LocationClientOption(); //以下是给定位设置参数
-        option.setOpenGps(true); // 打开gps
-        option.setCoorType("bd09ll"); // 设置坐标类型
-        option.setScanSpan(1000);
-        mLocClient.setLocOption(option);
-        mLocClient.start();
+    }
+    @AfterPermissionGranted(500)
+    private void methodRequiresTwoPermission() {
 
+        if (EasyPermissions.hasPermissions(this, s)) {
+            mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NONE);
+            // 开启定位图层
+            mBaiduMap.setMyLocationEnabled(true);
+            mLocClient = new LocationClient(this);  //定位用到的一个类
+            mLocClient.registerLocationListener(myListener); //注册监听
+            ///LocationClientOption类用来设置定位SDK的定位方式，
+            LocationClientOption option = new LocationClientOption(); //以下是给定位设置参数
+            option.setOpenGps(true); // 打开gps
+            option.setCoorType("bd09ll"); // 设置坐标类型
+            option.setScanSpan(1000);
+            option.setIsNeedLocationDescribe(true);
+            option.setIsNeedAddress(true);
+            mLocClient.setLocOption(option);
+            mLocClient.start();
+        } else {
+            // Do not have permissions, request them now
+            EasyPermissions.requestPermissions(this,"dsafdf",
+                    500, s);
+        }
     }
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
     boolean isFirstLoc = true; // 是否首次定位
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+        if(perms.size()==5){
+            mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NONE);
+            // 开启定位图层
+            mBaiduMap.setMyLocationEnabled(true);
+            mLocClient = new LocationClient(this);  //定位用到的一个类
+            mLocClient.registerLocationListener(myListener); //注册监听
+            ///LocationClientOption类用来设置定位SDK的定位方式，
+            LocationClientOption option = new LocationClientOption(); //以下是给定位设置参数
+            option.setOpenGps(true); // 打开gps
+            option.setCoorType("bd09ll"); // 设置坐标类型
+            option.setScanSpan(1000);
+            option.setIsNeedLocationDescribe(true);
+            option.setIsNeedAddress(true);
+            mLocClient.setLocOption(option);
+            mLocClient.start();
+        }
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+
+    }
+
     public class MyLocationListenner implements BDLocationListener {
 
         @Override
@@ -79,16 +118,25 @@ public class MainActivity extends AppCompatActivity {
             if (location == null || mMapView == null) {
                 return;
             }
+            //检测状态码
+            int locType = location.getLocType();
+            String locTypeDescription = location.getLocTypeDescription();
+            String addrStr = location.getAddrStr();
+
+
             MyLocationData locData = new MyLocationData.Builder()
                     .accuracy(location.getRadius())
                     // 此处设置开发者获取到的方向信息，顺时针0-360
                     .direction(100).latitude(location.getLatitude())
                     .longitude(location.getLongitude()).build();
+
             mBaiduMap.setMyLocationData(locData);
             if (isFirstLoc) {
                 isFirstLoc = false;
                 LatLng ll = new LatLng(location.getLatitude(),
                         location.getLongitude());
+                       location.getLocationDescribe();
+
                 MapStatus.Builder builder = new MapStatus.Builder();
                 builder.target(ll).zoom(18.0f);
                 mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
@@ -110,8 +158,6 @@ public class MainActivity extends AppCompatActivity {
         mMapView.onDestroy();
         mMapView = null;
     }
-
-
 
     @Override
     protected void onResume() {
